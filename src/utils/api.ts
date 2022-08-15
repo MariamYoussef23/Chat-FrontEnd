435b075be5f85e56e2383b4c756d1a3a21f94d9d
FahadAlshemali <fhadfhad222@gmail.com>
2022-08-15 09:42:25 +0200

    edit

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,21 +1,20 @@
 import axios from "axios";
 
-const API = axios.create({baseURL:"http://localhost:5000/users" })
+const API = axios.create({ baseURL: "http://localhost:5000/users" });
 
 export const signUpAPI = async (values: Object) => {
-    try {
-        const response = await API.post("/signup", values) 
-        //saving response to local storage 
-        localStorage.setItem('token', JSON.stringify(response.data.token))
-    }catch (error){
-        console.log(error)
-    }
-
-}
+  try {
+    const response = await API.post("/signup", values);
+    //saving response to local storage
+    localStorage.setItem("token", JSON.stringify(response.data.token));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const loginAPI = async (values: object) => {
   try {
-    const response = await axios.post(`/login`, values);
+    const response = await API.post(`/login`, values);
     console.log(response);
   } catch (error) {
     console.log(error);
