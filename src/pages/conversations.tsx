41a08d517bc944804d7673379d84d386edd5536a
FahadAlshemali <fhadfhad222@gmail.com>
2022-08-22 10:56:30 +0200

    adding create chat modal

--- src/pages/conversations.tsx
+++ src/pages/conversations.tsx
@@ -1,9 +1,12 @@
-import { Image } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form, Image, InputGroup, Modal } from "react-bootstrap";
 import ChatList from "../components/chatList";
 import backgroundImg from "../images/bg-img.jpg";
 
 const Conversations = () => {
-  
+  const [smShow, setSmShow] = useState(false);
+  const [lgShow, setLgShow] = useState(false);
+
   return (
     <>
       <div
@@ -11,6 +14,41 @@ const Conversations = () => {
         className="text-start ps-5"
       >
         <h1>Chat</h1>
+        <Button onClick={() => setSmShow(true)} className="me-2">
+          Small modal
+        </Button>
+        <Modal
+          size="sm"
+          show={smShow}
+          onHide={() => setSmShow(false)}
+          aria-labelledby="example-modal-sizes-title-sm"
+        
+        >
+          <Modal.Header
+           closeButton>
+            <Modal.Title id="example-modal-sizes-title-sm">
+              Small Modal
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body >
+            <InputGroup size="sm" className="mb-3">
+              <InputGroup.Text id="inputGroup-sizing-sm">Chat Name</InputGroup.Text>
+              <Form.Control
+                aria-label="Small"
+                aria-describedby="inputGroup-sizing-sm"
+              />
+            </InputGroup>
+            <br />
+            <InputGroup size="lg">
+              <InputGroup.Text id="inputGroup-sizing-lg">test</InputGroup.Text>
+              <Form.Control
+                aria-label="Large"
+                aria-describedby="inputGroup-sizing-sm"
+              />
+            </InputGroup>
+          </Modal.Body>
+          <Button variant="secondary">Submit</Button>
+        </Modal>
       </div>
 
       <div
